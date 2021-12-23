d7050f40b9f44ffaacfdee36bcdde4d957b57602
Doan Sy Hung <hungdoansy@gmail.com>
2021-12-23 17:33:04 +0700

    Initial commit

---
+++ src/mapping.ts
@@ -0,0 +1,5 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+import { Transfer } from "../generated/Token/Token";
+import { TransferTx } from "../generated/schema";
+
+export function handleTransfer(event: Transfer): void {}
