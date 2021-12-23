3b6b5ef8e128f4f7f13c642968bfe8d9b550d3fb
Doan Sy Hung <hungdoansy@gmail.com>
2021-12-23 18:16:45 +0700

    feat: store transfer events of TetherUSDT on ropsten

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,5 +1,20 @@
-import { BigInt } from "@graphprotocol/graph-ts";
-import { Transfer } from "../generated/Token/Token";
-import { TransferTx } from "../generated/schema";
+import { BigDecimal, BigInt } from "@graphprotocol/graph-ts";
+import { Transfer as TransferEvent } from "../generated/Token/Token";
+import { Transfer } from "../generated/schema";
 
-export function handleTransfer(event: Transfer): void {}
+export function handleTransfer(event: TransferEvent): void {
+  let transactionHash = event.transaction.hash.toHexString();
+  let transfer = new Transfer(transactionHash);
+
+  let from = event.params.from;
+  let to = event.params.to;
+
+  transfer.from = from;
+  transfer.to = to;
+
+  transfer.amount = event.params.value
+    .toBigDecimal()
+    .div(BigDecimal.fromString("1000000"));
+
+  transfer.save();
+}
